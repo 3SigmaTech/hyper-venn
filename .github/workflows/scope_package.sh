713c9ff4f71320034d7dc651334e5bb951c45243
3SigmaTech <32991725+3SigmaTech@users.noreply.github.com>
2023-03-02 13:54:24 -0500

    Iterating on a theme here

--- .github/workflows/scope_package.sh
+++ .github/workflows/scope_package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 YOUR_FILE='package.json'
-FINDTEXT="\\\"name\\\": \\\"hyper-venn\\\""
-SUBSTEXT="\\\"name\\\": \\\"@3SigmaTech\/hyper-venn\\\""
+FINDTEXT="\"name\": \"hyper-venn\""
+SUBSTEXT="\"name\": \"@3SigmaTech\/hyper-venn\""
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i "" "s/$FINDTEXT/$SUBSTEXT/g" $YOUR_FILE
