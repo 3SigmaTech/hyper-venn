686c0e7b4cd2a9b4b557b0a6a0089ad30ce3555c
3SigmaTech <32991725+3SigmaTech@users.noreply.github.com>
2023-03-02 13:42:02 -0500

    Iterating on a theme here

--- .github/workflows/scope_package.sh
+++ .github/workflows/scope_package.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 YOUR_FILE='package.json'
-FINDTEXT="\"name\": \"hyper-venn\""
-SUBSTEXT="\"name\": \"@3SigmaTech\/hyper-venn\""
+FINDTEXT="\\\"name\\\": \\\"hyper-venn\\\""
+SUBSTEXT="\\\"name\\\": \\\"@3SigmaTech\/hyper-venn\\\""
 sed -i "" "s/$FINDTEXT/$SUBSTEXT/" $YOUR_FILE
