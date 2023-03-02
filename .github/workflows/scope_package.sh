d595d366cf3efea4e0070e604737e355b112a142
3SigmaTech <32991725+3SigmaTech@users.noreply.github.com>
2023-03-02 14:06:54 -0500

    Iterating on a theme here

--- .github/workflows/scope_package.sh
+++ .github/workflows/scope_package.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 YOUR_FILE='package.json'
+FINDTEXT="\\\"name\\\": \\\"hyper-venn\\\""
+SUBSTEXT="\\\"name\\\": \\\"@3SigmaTech\/hyper-venn\\\""
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  FINDTEXT="\\\"name\\\": \\\"hyper-venn\\\""
-  SUBSTEXT="\\\"name\\\": \\\"@3SigmaTech\/hyper-venn\\\""
   sed -i "" "s/$FINDTEXT/$SUBSTEXT/g" $YOUR_FILE
 else
-  sed -i $YOUR_FILE '0,/hyper-venn/{s/hyper-venn/@3SigmaTech\/hyper-venn/}'
+  sed -i "s/$FINDTEXT/$SUBSTEXT/g" $YOUR_FILE
 fi
